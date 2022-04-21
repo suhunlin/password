721a39c8f7a4fbb372869ff0ea8559e311e60b97
suhunlin <lin.suhun@msa.hinet.net>
2022-04-21 18:26:54 +0800

    modify output message

--- user_password.py
+++ user_password.py
@@ -1,11 +1,13 @@
 password = 'a123456'
 count = 3
 while count > 0:
+    count -= 1
     user_input = input('Please input password:')
     if password == user_input:
         print("Correct!!")
         break
     else:
-        count -= 1
-        print('Input error!!!You still have %d chance' %count)
+        print('Input error!!!')
+        if count > 0:
+            print("You still have %d chance!!!" %count)
 
